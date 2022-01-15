3785dfedbdb2d11dbdaed7a0fdb0a41af77a52fe
Mohammadreza <mrvashian@gmail.com>
2022-01-15 20:57:33 +0330

    adding application configuration

--- authz/authz.py
+++ authz/authz.py
@@ -1,6 +1,13 @@
-from flask import Flask
+from flask import Flask, Blueprint
+from flask_restful import Api
+
+from authz.config import Config
+
+apiv1_bp = Blueprint("apiv1_bp", __name__, url_prefix='/api/v1')
+apiv1 = Api(apiv1_bp)
 
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(Config)
     return app
